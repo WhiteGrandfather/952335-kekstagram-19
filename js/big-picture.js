33435a4e8f5fbac844ca07860a135b506c8b23e9
Михаил <santa_dj@mail.ru>
2020-03-20 12:35:19 +0300

    Закончил Личный проект: перламутровые пуговицы (часть 2)

--- js/big-picture.js
+++ js/big-picture.js
@@ -4,57 +4,87 @@
   var INLINE_COMMENTS_COUNT = 2;
   var INLINE_COMMENT_REMOVE = 1;
   var MAX_COMMENTS = 5;
+  var commentsShow = MAX_COMMENTS;
   var bigPictureElement = document.querySelector('.big-picture');
   var bigCommentsListElement = bigPictureElement.querySelector('.social__comments');
   var bodyElement = document.querySelector('body');
   var bigComments = bigCommentsListElement.children;
+  var commentsLoader = bigPictureElement.querySelector('.comments-loader');
 
   var getBigComment = function () {
-    var bigComment = bigComments[0];
-    var comentElement = bigComment.cloneNode(true);
+    var comentElement = bigComments[0].cloneNode(true);
 
     return comentElement;
   };
 
-  var getBigComments = function (comments) {
-    var fragmentBigPicture = document.createDocumentFragment();
-
+  var removeBigComments = function (comments) {
     if (comments.comments.length < INLINE_COMMENTS_COUNT) {
       bigComments[INLINE_COMMENT_REMOVE].remove('li');
+    } else {
+      for (var k = INLINE_COMMENT_REMOVE; k < bigComments.length; k++) {
+        bigComments[k].remove('li');
+      }
     }
+  };
 
-    for (var i = INLINE_COMMENTS_COUNT; i < MAX_COMMENTS; i++) {
+  var getBigComments = function (comments) {
+    var fragmentBigPicture = document.createDocumentFragment();
+    commentsShow = MAX_COMMENTS;
+    removeBigComments(comments);
+
+    for (var i = bigComments.length; i < comments.comments.length; i++) {
       fragmentBigPicture.appendChild(getBigComment());
     }
 
     return fragmentBigPicture;
   };
 
+  var renderComments = function (element, elementLength) {
+    for (var i = 0; i < elementLength; i++) {
+      bigComments[i].querySelector('.social__picture').src = element[i].avatar;
+      bigComments[i].querySelector('.social__picture').alt = element[i].name;
+      bigComments[i].querySelector('.social__text').textContent = element[i].message;
+      bigComments[i].style.display = '';
+    }
+  };
+
+  var onCommentsLoader = function () {
+    commentsShow = commentsShow + 5;
+    for (var i = 0; i < commentsShow; i++) {
+      bigComments[i].style.display = 'flex';
+    }
+  };
+
   var renerBigPicture = function (index) {
-    var getMinPictures = function (pictures) {
-      bigPictureElement.querySelector('.big-picture__img').querySelector('img').src = pictures[index].url;
-      bigPictureElement.querySelector('.likes-count').textContent = pictures[index].likes;
-      bigPictureElement.querySelector('.comments-count').textContent = pictures[index].comments.length;
-      bigPictureElement.querySelector('.social__caption').textContent = pictures[index].description;
+    var pictures = window.minPictures.pictureListCopy;
 
-      bigCommentsListElement.appendChild(getBigComments(pictures[index]));
+    bigPictureElement.querySelector('.big-picture__img').querySelector('img').src = pictures[index].url;
+    bigPictureElement.querySelector('.likes-count').textContent = pictures[index].likes;
+    bigPictureElement.querySelector('.comments-count').textContent = pictures[index].comments.length;
+    bigPictureElement.querySelector('.social__caption').textContent = pictures[index].description;
 
-      var element = pictures[index].comments;
+    bigCommentsListElement.appendChild(getBigComments(pictures[index]));
 
-      element.forEach(function (item, i) {
-        bigComments[i].querySelector('.social__picture').src = item.avatar;
-        bigComments[i].querySelector('.social__picture').alt = item.name;
-        bigComments[i].querySelector('.social__text').textContent = item.message;
-      });
-    };
-    window.backend.load(getMinPictures);
+    var commentsElement = pictures[index].comments;
+
+    if (commentsElement.length > commentsShow) {
+      renderComments(commentsElement, commentsElement.length);
+      for (var i = commentsShow; i < bigComments.length; i++) {
+        bigComments[i].style.display = 'none';
+      }
+    } else {
+      renderComments(commentsElement, commentsElement.length);
+    }
+
+    commentsLoader.addEventListener('click', onCommentsLoader);
   };
 
   bigPictureElement.querySelector('.social__comment-count').classList.add('hidden');
   bodyElement.classList.add('modal-open');
 
   window.bigPicture = {
-    renerBigPicture: renerBigPicture
+    renerBigPicture: renerBigPicture,
+    onCommentsLoader: onCommentsLoader
   };
 
 })();
